efa9efb504a101ecb4c146adc40c564be0304b9a
JOSHUA ZYNDORF <zyndorf.1@osu.edu>
2018-01-27 19:37:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
 var recipes = {}
 
-function updateObjectWithKeyAndValue(first, key, value){
-  return Object.assign({}, first, {prop: 1, prop2: 2 })
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(second, key, value) {
-  second[key] = value
-  return second;
-}
-
-function deleteFromObjectByKey(object, key) {
-  var clone = object[key]
-  return clone
-}
-
-function destructivelyDeleteFromObjectByKey(object, key) {
- delete object[key]
- return object
+function updateObjectWithKeyAndValue(object, key, value) {
+  object = key['value']
+  return object
 }
