5f5be27acee14631eed71ab235d7e54e100bdbf7
JOSHUA ZYNDORF <zyndorf.1@osu.edu>
2018-01-27 19:55:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   var googoo = {[key]: value}
-  return object[googoo]
+  return googoo
 }
