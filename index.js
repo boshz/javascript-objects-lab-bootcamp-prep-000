d92806d3e47c59bb9edc35dbefabaaedfb3291be
JOSHUA ZYNDORF <zyndorf.1@osu.edu>
2018-01-27 20:10:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 
+function deleteFromObjectByKey(object, key) {
+  var goober = object[key]
+  goober.delete(key)
+  return goober
+}
